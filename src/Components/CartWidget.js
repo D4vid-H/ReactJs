84cdc47eda817398eff5f137b1ae0303c10aacaf
David Hillton <davidhillton@gmail.com>
2022-03-23 20:04:53 -0300

    add Navbar & img & component cartwidget

---
+++ src/Components/CartWidget.js
@@ -0,0 +1,12 @@
+import changuito from '../img/changuito.png'
+import '../Css/CartWidget.css'
+
+function CartWidget(){
+
+return(
+    <div className='changuito'>
+        <img src={changuito} className='changuito' />
+    </div>
+)}
+
+export default CartWidget;
